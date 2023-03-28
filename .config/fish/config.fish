39c30f6e3bc5b854462e2ddb3d69a88be8f958d2
Manuel Cabos <cabos.apolaya@gmail.com>
2023-03-28 11:32:56 -0500

    Add .config/fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+# Comandos para configurar  oh-my-fish
+set -g theme_display_user yes                 # Mostrar usuario
+set -g fish_prompt_pwd_dir_length -1          # Mostrar path completo
+set -g theme_display_git_default_branch yes   # Mostrar branch
+set -g theme_newline_cursor yes               # Escribir comando una linea abajo
+set -g theme_newline_prompt '> '              # Icono de la nueva linea
+set -g theme_color_scheme dark                # Tema
